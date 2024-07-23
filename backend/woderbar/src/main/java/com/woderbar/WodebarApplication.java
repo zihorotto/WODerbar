6180934e801bb7b96a1ab13fcb2749fbefd3b05a
ottozihor <otto.zihor.woderbar@gmail.com>
2024-07-23 13:49:21 +0200

    Starter pack

---
+++ backend/woderbar/src/main/java/com/woderbar/WodebarApplication.java
@@ -0,0 +1,13 @@
+package com.wodebar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoderbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoderbarApplicationbar.class, args);
+	}
+
+}
